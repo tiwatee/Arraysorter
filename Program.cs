221ac917e3fb22731db66ab9fa7666c74ded8948
tiwatee <kriswendie4eva@gmail.com>
2022-08-04 19:43:49 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ArraySort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] unsortedList = {100,2,4,1,38,18,34,22,10,63,12}; 
+            //unsorted list.
+            //Sorted:63,38,34,22,18,12,10,4,2,1
+            Console.WriteLine("The unsorted list is [{0}]", string.Join(",", unsortedList));
+            Sort sortedList = new Sort();
+            int[] output = sortedList.SortedArray(unsortedList);
+            Console.WriteLine("The sorted list is [{0}]", string.Join(",", output));
+        }
+    }
+
+}
---
+++ Sort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySort
+{
+    internal class Sort
+    {
+        public int[] SortedArray(int[] unsortedList)
+        {
+            int min = unsortedList[0];
+            for (int j = unsortedList.Length; j >= 0; j--)
+            {
+                for (int i = 1; i < j; i++)
+                {
+                    int a = unsortedList[i - 1];
+                    int b = unsortedList[i];
+                    if (a < b)
+                    {
+                        unsortedList[i] = a;
+                        unsortedList[i - 1] = b;
+                    }
+                }
+            }
+            return unsortedList;
+        }
+    }
+}
